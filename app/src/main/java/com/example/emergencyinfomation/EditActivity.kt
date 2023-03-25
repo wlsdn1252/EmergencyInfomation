ab50eabcca9ec7daf16736e19463053e1345c886
강진우 <wlsdn1252@naver.com>
2023-03-25 21:05:11 +0900

    작성한 데이터를 저장하는 코드 작성

--- app/src/main/java/com/example/emergencyinfomation/EditActivity.kt
+++ app/src/main/java/com/example/emergencyinfomation/EditActivity.kt
@@ -2,9 +2,11 @@ package com.example.emergencyinfomation
 
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_edit.*
 
@@ -41,6 +43,37 @@ class EditActivity : AppCompatActivity() {
         // 체크박스 기본값 세팅
         warningEditText.isVisible = warningCheckBox.isChecked
 
+        // 저장버튼 클릭시 데이터 저장
+        saveButton.setOnClickListener{
+            saveDate()
+            finish()
+        }
+
+    }
+    // 저장버튼 클릭시 데이터 저장 함수
+    private fun saveDate(){
+        // 코드의 반복을 줄이기 위해 whit()을 쓴다.
+        with( getSharedPreferences("userInformation",Context.MODE_PRIVATE).edit()){
+            putString("name",nameEditText.text.toString())
+            putString("bloodType",getBloodType())
+            putString("emergencyContact",emergencyContactEditText.text.toString())
+            putString("birthDate", birthdateTextView.text.toString())
+            putString("warning",getWarning())
+            apply()
+        }
+        Toast.makeText(this, "저장완료", Toast.LENGTH_SHORT).show()
+    }
+
+    // 혈액형 데이터를 받기 위한 함수
+    private fun getBloodType() :String {
+        val bloodAlphabet = bloodTypeSpinner.selectedItem.toString()
+        val bloodSign = if(bloodTypePlus.isChecked) "+" else "-"
+        return "$bloodSign$bloodAlphabet"
+    }
+
+    //
+    private fun getWarning() : String{
+        return if(warningCheckBox.isChecked) warningEditText.text.toString() else " "
     }
 
 }
